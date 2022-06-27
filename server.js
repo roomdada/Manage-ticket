ec0dcfcea9d42edb55a8b89ce0f8446c5c47ab83
Roomdada <dsieroger@gmail.com>
2022-06-27 12:28:11 +0000

    :tada: Initial commit

---
+++ server.js
@@ -0,0 +1,19 @@
+const app = require('express')()
+
+app.set('view engine', 'ejs')
+
+
+app.get('/', (req, res) => {
+  res.render('pages/index');
+});
+
+app.get('/about', (req, res) => {
+  res.render('pages/about');
+});
+
+app.get('/contact', (req, res) => {
+  res.render('pages/contact');
+});
+
+
+app.listen(3000);
